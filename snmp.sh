024ee9e9e320eaf52052b1d579350ec472d34e4a
ubuntu <ubuntu@isb-server01.novalocal>
2018-04-26 04:35:36 +0000

    first commit

---
+++ snmp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+COMMUNITY=$1
+OID="1.3.6.1.4.1.2636.3.1.13.1.8.9.1.0.0"
+DIR="/home/ubuntu/"
+ 
+#NUM=$1
+#OCT=$(( ${NUM} + 150 ))
+#TARGET="10.10.1.${OCT}"
+#DATE=$(date +%Y%m%d_%H%M)
+#LOG_FILE="vSRX_snmp_log_${DATE}.txt"
+#FILE_PATH=/home/ubuntu
+TARGET=$1
+SLEEP=1
+SEQ=1
+ 
+#while :; do 
+#    echo -n "$(date) vSRX_snmpget $TARGET ">>${DIR}${LOG_FILE}; 
+#    /usr/bin/snmpget -c $COMMUNITY -v2c $TARGET $OID >>${FILE_PATH}/${LOG_FILE} 2>&1; 
+#    if [ $? -ne 0 ]; then
+#        exit 1
+#    fi
+#    sleep $SLEEP; 
+#done &
+
+while :; do 
+    echo -n "$(date) vSRX_snmpget $TARGET seq=$SEQ "; 
+    /usr/bin/snmpget -c $COMMUNITY -v2c $TARGET $OID 2>&1; 
+    SEQ=$(( SEQ + 1 ))
+    sleep $SLEEP; 
+done 
